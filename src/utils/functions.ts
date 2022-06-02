301ee07dd48e468b43ee66f2c2d61c481951883b
Diego Sousa <diegointeriores11@gmail.com>
2022-06-02 17:23:35 -0300

    feat: adjust regex

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,2 +1,2 @@
-export const regexReplaceSpacesLowercase = (text: string) => text.replace(/ */gm, '').toLowerCase();
+export const regexReplaceSpacesLowercase = (text: string) => text.replace(/ *[.?!,~]/gm, '').toLowerCase();
 export const regexMatchReplaceUser = (text: string, userName: string) => text.replace(/\{\user}/gm, userName);
